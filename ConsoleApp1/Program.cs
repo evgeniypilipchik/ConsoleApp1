1e501c9ad40d9715ccefe1aa3b8205e619b0d777
Evegniy Pilipchik <evgeniypilipchik@yandex.ru>
2025-03-07 22:35:33 +0300

    Construction if

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число");
+            int numb1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число");
+            int numb2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите оператор (+ - * / %)");
+            string oper = Console.ReadLine();
+            if (oper == "+")
+            {
+                Console.WriteLine(numb1 + numb2);
+            }
+            else if (oper == "-")
+            {
+                Console.WriteLine(numb1 - numb2);
+            }
+            else if (oper == "*")
+            {
+                Console.WriteLine(numb1 * numb2);
+            }
+            else if (oper == "/")
+            {
+                Console.WriteLine(numb1 / numb2);
+            }
+            else if (oper == "%")
+            {
+                Console.WriteLine(numb1 % numb2);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, следуйте инструкции");
+            }
+        }
+    }
+}
