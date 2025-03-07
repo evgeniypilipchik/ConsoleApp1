5e933a380338d5e61122d37bfc47bfcf796d2d44
Evegniy Pilipchik <evgeniypilipchik@yandex.ru>
2025-03-07 22:39:04 +0300

    Construction switch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,49 @@
-﻿namespace ConsoleApp1
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое целое число");
-            int numb1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе целое число");
-            int numb2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите оператор (+ - * / %)");
-            string oper = Console.ReadLine();
-            if (oper == "+")
+            while (true)
             {
-                Console.WriteLine(numb1 + numb2);
+                Console.WriteLine("Введите первое целое число");
+                double numb1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второе целое число");
+                double numb2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите оператор (+ - * / %)");
+                string oper = Console.ReadLine();
+                double result = 0;
+                switch (oper)
+                {
+                    case "+":
+                        result = numb1 + numb2;
+                        break;
+
+                    case "-":
+                        result = numb1 - numb2;
+                        break;
+
+                    case "*":
+                        result = numb1 * numb2;
+                        break;
+
+                    case "/":
+                        result = numb1 / numb2;
+                        break;
+
+                    case "%":
+                        result = numb1 % numb2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка, следуйте инструкции");
+                        break;
+                }
+                Console.WriteLine("Результат: " + result);
             }
-            else if (oper == "-")
-            {
-                Console.WriteLine(numb1 - numb2);
-            }
-            else if (oper == "*")
-            {
-                Console.WriteLine(numb1 * numb2);
-            }
-            else if (oper == "/")
-            {
-                Console.WriteLine(numb1 / numb2);
-            }
-            else if (oper == "%")
-            {
-                Console.WriteLine(numb1 % numb2);
-            }
-            else
-            {
-                Console.WriteLine("Ошибка, следуйте инструкции");
-            }
+
         }
     }
 }
